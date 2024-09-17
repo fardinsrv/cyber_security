4c90c80ce27bbe45a5e08d44f4e4e222862032c3
Fardin Saurov <fardintutul@gmail.com>
2024-09-17 12:21:18 +0600

    Update numberTOtext.py

--- numberTOtext.py
+++ numberTOtext.py
@@ -5,7 +5,8 @@ for i in x:
     tuplee[num +1] = i
     num += 1
 
-
+# it will make a tuple like this {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j',
+ #   11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'}
 
 def numberTOtext(num):
     encoded_text =  num
